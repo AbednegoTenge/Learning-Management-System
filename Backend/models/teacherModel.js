7b2e49eb83a1867093da493b0679ad6e72108830
AbednegoTenge <abednegotenge180@gmail.com>
2025-07-26 20:25:22 +0000

    adding subject field to model

--- Backend/models/teacherModel.js
+++ Backend/models/teacherModel.js
@@ -25,6 +25,10 @@ Teacher.init({
         type: DataTypes.STRING,
         allowNull: false,
     },
+    subject: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,

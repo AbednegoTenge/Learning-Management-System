81981d02d6ded58544e4734004b3d3666ffa4921
AbednegoTenge <abednegotenge180@gmail.com>
2025-08-07 10:43:34 +0000

    syncing sequelize

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import logger from './logger.js';
+import { generalLogger } from './logger.js';
+import './models/studentModel.js';
+import './models/teacherModel.js';
+import './models/courseModel.js';
+import './models/studentCourseModels.js';
+import sequelize from './config/sequelize.js';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+try {
+    const app = express();
+    const PORT = process.env.PORT || 3000;
+    
 
-app.listen(PORT, () => {
-    logger.info(`Server is running on port ${PORT}`);
-});
+    // Initialize Sequelize
+    sequelize.sync({ force: false })
+        .then(() => {
+            generalLogger.info('Database synchronized successfully');
+        })
+        .catch((error) => {
+            generalLogger.error(`Error synchronizing database: ${error.message}`, { stack: error.stack });
+        });
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    
+
+    app.listen(PORT, () => {
+        generalLogger.info(`Server is running on port ${PORT}`);
+    });
+} catch (error) {
+    generalLogger.error(`Error starting the server: ${error.message}`, { stack: error.stack });
+}

a79422d9f5682b381f97882400e904aadc70ccd1
AbednegoTenge <abednegotenge180@gmail.com>
2025-08-07 10:37:19 +0000

    changing resetToken to refreshToken

--- Backend/utils/resetToken.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-
-export function generateResetToken() {
-    const token = crypto.randomBytes(32).toString('hex');
-    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
-    const expiresAt = Date.now() + 15 * 60 * 1000 // 15 minutes expiry
-    return { token: hashedToken, expiresAt };
-};
